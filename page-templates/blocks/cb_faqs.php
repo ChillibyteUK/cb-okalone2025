245d9d7fe72df88bf66056f76f147a94a49863fe
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-02-13 15:08:43 +0000

    faqs

--- page-templates/blocks/cb_faqs.php
+++ page-templates/blocks/cb_faqs.php
@@ -4,12 +4,12 @@
         <?php
         if(get_field('faq_title')) {
             ?>
-        <h2 class="mb-4 text-center"><?=get_field('faq_title')?></h2>
+        <h2 class="mb-4 text-start"><?=get_field('faq_title')?></h2>
             <?php
         }
         if(get_field('faq_intro')) {
             ?>
-        <p class="text-center mw-md-75 mb-4"><?=get_field('faq_intro')?></p>
+        <p class="text-start mw-md-75 mb-4"><?=get_field('faq_intro')?></p>
             <?php
         }
         ?>
@@ -28,7 +28,7 @@
         ?>
         <div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
             <div class="accordion-head accordion-collapse <?=$collapsed?>" itemprop="name" data-bs-toggle="collapse" id="heading_<?=$ac?>" data-bs-target="#c<?=$ac?>" role="button" aria-expanded="true" aria-controls="c<?=$ac?>">
-                <div class="pb-1"><?=get_sub_field('question')?></div>
+                <div class="pb-1 fw-900"><?=get_sub_field('question')?></div>
             </div>
             <div class="collapse <?=$show?>" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" id="c<?=$ac?>" aria-labelledby="heading_<?=$ac?>" data-bs-parent="#accordion<?=$accordion?>">
                 <div itemprop="text" class="faq__answer mb-4">
