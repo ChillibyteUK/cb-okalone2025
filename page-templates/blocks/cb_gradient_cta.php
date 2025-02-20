8cd78e5604a20a3ef15e1662405bc2e3cd7a2dd7
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-02-20 10:10:43 +0000

    resources video

--- page-templates/blocks/cb_gradient_cta.php
+++ page-templates/blocks/cb_gradient_cta.php
@@ -1,16 +1,20 @@
 <?php
-$grad = get_field('theme');
-$cta = get_field('cta') ?? null;
+$grad = get_query_var('grad', get_field('theme'));
+$cta = get_query_var('cta', get_field('cta') ?? null);
+$title = get_query_var('title', get_field('title'));
+$content = get_query_var('content', get_field('content'));
+$modal_trigger = get_query_var('modal_trigger', get_field('modal_trigger'));
+
 ?>
 <!-- gradient_cta -->
 <section class="gradient_cta py-5 bg_grad--<?= $grad ?>">
     <div class="container">
         <div class="row">
             <div class="col-lg-7 offset-lg-5" data-aos="fade-left">
-                <h2><?= get_field('title') ?></h2>
-                <div class="mb-4"><?= get_field('content') ?></div>
+                <h2><?= $title ?></h2>
+                <div class="mb-4"><?= $content ?></div>
                 <?php
-                $modal_trigger = get_field('modal_trigger');
+                $modal_trigger = $modal_trigger;
                 if (is_array($modal_trigger) && isset($modal_trigger[0]) && $modal_trigger[0] == 'Yes') {
                 ?>
                     <button type="button" class="button button-outline" data-bs-toggle="modal" data-bs-target="#demoModal">Book a Demo</button>
--- page-templates/cb_resources.php
+++ page-templates/cb_resources.php
@@ -37,8 +37,7 @@ get_header();
             }
             ?>
         </div>
-        <?php
-        ?>
+
         <h2>Blog</h2>
         <div class="latest_blogs pb-5 row g-4">
             <?php
@@ -70,9 +69,90 @@ get_header();
             }
             ?>
         </div>
+
+        <h2>How To</h2>
+        <div class="how_to pb-5 row g-4">
+            <?php
+            $q = new WP_Query(array(
+                'post_type'      => 'post',
+                'posts_per_page' => 4,
+                'tax_query'      => array(
+                    array(
+                        'taxonomy' => 'article-type',
+                        'field'    => 'slug', // You can also use 'term_id' if you prefer
+                        'terms'    => 'how-to', // The slug of the term
+                    ),
+                ),
+            ));
+            while ($q->have_posts()) {
+                $q->the_post();
+                ?>
+                <div class="col-md-6">
+                    <a href="<?=get_the_permalink()?>" class="how_to__card">
+                        <?=get_the_post_thumbnail($q->ID,'large',['class' => 'how_to__image'])?>
+                        <div class="how_to__inner">
+                            <h3><?=get_the_title()?></h3>
+                            <div class="how_to__excerpt <?=$mb?>"><?=wp_trim_words(get_the_content(null,false,$q->ID),12)?></div>
+                        </div>
+                    </a>
+                </div>
+                <?php
+                $first = false;
+            }
+            ?>
+        </div>
         <?php
+        // there are no guides
         ?>
+        <h2>Videos</h2>
+        <div class="videos pb-5 row g-4">
+            <?php
+            $q = new WP_Query(array(
+                'post_type'      => 'post',
+                'posts_per_page' => 4,
+                'tax_query'      => array(
+                    array(
+                        'taxonomy' => 'article-type',
+                        'field'    => 'slug', // You can also use 'term_id' if you prefer
+                        'terms'    => 'video', // The slug of the term
+                    ),
+                ),
+            ));
+            while ($q->have_posts()) {
+                $q->the_post();
+                ?>
+                <div class="col-md-4">
+                    <a href="<?=get_the_permalink()?>" class="videos__card">
+                        <div class="videos__image_box">
+                            <?=get_the_post_thumbnail($q->ID,'large',['class' => 'videos__image'])?>
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icons/play-button.png" class="play" width="59" height="59">
+                        </div>
+                        <div class="videos__inner">
+                            <h3><?=get_the_title()?></h3>
+                            <div class="videos__excerpt <?=$mb?>"><?=wp_trim_words(get_the_content(null,false,$q->ID),12)?></div>
+                        </div>
+                    </a>
+                </div>
+                <?php
+                $first = false;
+            }
+            ?>
+        </div>
     </div>
+    <?php
+    set_query_var('grad', 'orange'); // Example value for 'theme'
+    set_query_var('cta', array(
+        'url' => '#',
+        'target' => 'self',
+        'title' => 'Get a Free Trial'
+    ));
+    set_query_var('title', 'Book a Demo Today');
+    set_query_var('content', '<p>Protect your people with our dedicated personal safety service for at-risk and lone workers. Combining leading technology with a professional 24/7 Safety Monitoring Center, you can ensure your people get immediate emergency help when they need it most.
+</p><h3>Alternatively, get a free trial of the app</h3><p>Want to try OK Alone? Click the button below and enter your details. It\'s free and no credit card is required.</p>');
+    set_query_var('modal_trigger', array('Yes'));
+    
+    get_template_part('page-templates/blocks/cb_gradient_cta');
+    ?>
  </main>
 <?php
 
