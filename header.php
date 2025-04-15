5df29e6edfed2a66a1e95415acdf98a55341e929
ChillibyteUK <dan@chillibyte.co.uk>
2025-04-15 14:10:08 +0000

    nav speling

--- header.php
+++ header.php
@@ -300,7 +300,7 @@ defined('ABSPATH') || exit;
                                     <li><a href="/sectors/electricians/">Electricians</a></li>
                                     <li><a href="/sectors/hvac-employees/">HVAC Employees</a></li>
                                     <li><a href="/sectors/logistics-and-drivers/">Logistics and Drivers</a></li>
-                                    <li><a href="/sectors/municiapl-government/">Municipal/Government</a></li>
+                                    <li><a href="/sectors/municipal-government/">Municipal/Government</a></li>
                                     <li><a href="/sectors/office-and-retail-staff/">Office and Retail Staff</a></li>
                                     <li><a href="/sectors/security-companies/">Security Companies</a></li>
                                     <li><a href="/sectors/">All Sectors</a></li>
