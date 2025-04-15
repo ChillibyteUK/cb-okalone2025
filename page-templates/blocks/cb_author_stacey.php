f028894a66af9132698fd86e84f8a7019241e387
ChillibyteUK <dan@chillibyte.co.uk>
2025-04-15 15:52:22 +0000

    author block

--- page-templates/blocks/cb_author_stacey.php
+++ page-templates/blocks/cb_author_stacey.php
@@ -2,7 +2,7 @@
 $author_page = get_field( 'stacey_page', 'option' );
 ?>
 <section class="author py-5">
-	<div class="container-xl p-4 has-light-background-color">
+	<div class="container-xl p-4 has-light-background-color rounded-3 shadow-sm">
 		<div class="row g-5 justify-content-center">
 			<div class="col-6 col-md-2">
 				<?= wp_get_attachment_image( get_field( 'stacey_photo', 'option' ), 'full', false, array( 'class' => 'img-fluid rounded-circle' ) ); ?>
