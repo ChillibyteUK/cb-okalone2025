163bdc5278743bb65bfdc74987f91f1c4c871ed8
ChillibyteUK <dan@chillibyte.co.uk>
2025-04-15 15:38:44 +0000

    author template

---
+++ page-templates/author_template.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* Template Name: Author Template
+*/
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+get_header();
+?>
+<main id="main" class="single-author">
+    <section class="breadcrumbs container-xl">
+        <?php
+		if ( function_exists( 'yoast_breadcrumb' ) ) {
+			yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+		}
+		?>
+    </section>
+    <div class="container-xl">
+		<h1><?= get_the_title(); ?></h1>
+		<?= get_the_post_thumbnail(get_the_ID(), 'large', array( 'class' => 'mx-auto mb-4 single-author__image' ) ); ?>
+		<?=get_the_content()?>
+		<h2>Latest Posts</h2>
+		<?php
+		$latest_posts = new WP_Query(
+			array(
+				'post_type' => 'post',
+				'posts_per_page' => 5,
+			)
+		);
+		if ( $latest_posts->have_posts() ) {
+			echo '<ul class="list-unstyled">';
+			while ( $latest_posts->have_posts() ) {
+				$latest_posts->the_post();
+				echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+			}
+			echo '</ul>';
+		} else {
+			echo '<p>No posts found.</p>';
+		}
+		?>
+	</div>
+</main>
+<?php
+get_footer();
+?>
