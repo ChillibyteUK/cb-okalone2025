916a415c0b8b1fc36cf20208a089d72ea3db748a
ChillibyteUK <dan@chillibyte.co.uk>
2025-04-15 15:33:31 +0000

    stacey author

--- page-templates/blocks/cb_author_stacey.php
+++ page-templates/blocks/cb_author_stacey.php
@@ -2,7 +2,7 @@
 	<div class="container-xl p-4 has-light-background-color">
 		<div class="row g-5 justify-content-center">
 			<div class="col-6 col-md-2">
-				<?= wp_get_attachment_image( get_field( 'stacey_image', 'option' ), 'full', false, array( 'class' => 'img-fluid rounded-circle' ) ); ?>
+				<?= wp_get_attachment_image( get_field( 'stacey_photo', 'option' ), 'full', false, array( 'class' => 'img-fluid rounded-circle' ) ); ?>
 			</div>
 			<div class="col-12 col-md-10">
 				<h2>Stacey Manclark</h2>
