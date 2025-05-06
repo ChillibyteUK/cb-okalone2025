6a6a86fa70f45ee41d430d91a361bc02e934b6a9
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-05-06 15:57:39 +0100

    Update taxonomy-article-type.php

--- taxonomy-article-type.php
+++ taxonomy-article-type.php
@@ -16,13 +16,12 @@ if ($term_slug == 'video') {
 
 get_header();
 
-// Fix order and disable Post Types Order plugin
-global $wp_query;
-
-$wp_query->set('orderby', 'date');
-$wp_query->set('order', 'DESC');
-$wp_query->set('ignore_custom_sort', true);
-query_posts($wp_query->query);
+// Force correct order and disable Post Types Order
+query_posts(array_merge($wp_query->query, array(
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'ignore_custom_sort' => true
+)));
 ?>
 <main id="main" class="resources">
     <div class="container-xl pt-5">
@@ -30,7 +29,7 @@ query_posts($wp_query->query);
 
         <div class="taxonomy-description"><?php echo term_description(); ?></div>
 
-        <?php
+        <?php 
         if (have_posts()) {
             ?>
             <div class="<?php echo $section_class; ?>">
@@ -41,37 +40,37 @@ query_posts($wp_query->query);
 
                 if ($term_slug == 'video') {
                     ?>
-            <div class="col-md-4">
-                <a href="<?php echo get_the_permalink(); ?>" class="videos__card">
-                    <div class="videos__image_box">
-                        <?php echo get_the_post_thumbnail(get_the_ID(), 'large', array('class' => 'videos__image')); ?>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/play-button.png" class="play" width="59" height="59">
-                    </div>
-                    <div class="videos__inner">
-                        <h3><?php echo get_the_title(); ?></h3>
-                        <div class="videos__excerpt"><?php echo wp_trim_words(get_the_content(null, false, get_the_ID()), 12); ?></div>
-                    </div>
-                </a>
-            </div>
+                <div class="col-md-4">
+                    <a href="<?php echo get_the_permalink(); ?>" class="videos__card">
+                        <div class="videos__image_box">
+                            <?php echo get_the_post_thumbnail(get_the_ID(), 'large', array('class' => 'videos__image')); ?>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/play-button.png" class="play" width="59" height="59">
+                        </div>
+                        <div class="videos__inner">
+                            <h3><?php echo get_the_title(); ?></h3>
+                            <div class="videos__excerpt"><?php echo wp_trim_words(get_the_content(null, false, get_the_ID()), 12); ?></div>
+                        </div>
+                    </a>
+                </div>
                     <?php
                 } elseif ($term_slug == 'how-to') {
                     ?>
-            <div class="col-md-6">
-                <a href="<?php echo get_the_permalink(); ?>" class="how_to__card">
-                    <?php echo get_the_post_thumbnail(get_the_ID(), 'custom-thumb-275x184', array('class' => 'how_to__image')); ?>
-                    <div class="how_to__inner">
-                        <h3><?php echo get_the_title(); ?></h3>
-                        <div class="how_to__excerpt"><?php echo wp_trim_words(get_the_content(null, false, get_the_ID()), 12); ?></div>
-                    </div>
-                </a>
-            </div>
+                <div class="col-md-6">
+                    <a href="<?php echo get_the_permalink(); ?>" class="how_to__card">
+                        <?php echo get_the_post_thumbnail(get_the_ID(), 'custom-thumb-275x184', array('class' => 'how_to__image')); ?>
+                        <div class="how_to__inner">
+                            <h3><?php echo get_the_title(); ?></h3>
+                            <div class="how_to__excerpt"><?php echo wp_trim_words(get_the_content(null, false, get_the_ID()), 12); ?></div>
+                        </div>
+                    </a>
+                </div>
                     <?php
                 } else {
                     ?>
-            <article>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <p><?php the_excerpt(); ?></p>
-            </article>
+                <article>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <p><?php the_excerpt(); ?></p>
+                </article>
                     <?php
                 }
             }
