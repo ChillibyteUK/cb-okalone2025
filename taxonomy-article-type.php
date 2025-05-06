935ed7b6d06bd355468258a73cf6e93e72f5ba4a
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-05-06 16:01:54 +0100

    Update taxonomy-article-type.php

--- taxonomy-article-type.php
+++ taxonomy-article-type.php
@@ -64,7 +64,7 @@ get_header();
                     else {
                         ?>
                 <article>
-                    <h2><?= $term_slug ?> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <p><?php the_excerpt(); ?></p>
                 </article>
                         <?php
