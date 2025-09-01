1047ad25e194aab8707fbedc4666fda80e664f16
Matthew Pont <matthew.pont@gmail.com>
2025-09-01 10:02:22 +0100

    Update header.php

--- header.php
+++ header.php
@@ -15,7 +15,6 @@ defined('ABSPATH') || exit;
 <html <?php language_attributes(); ?>>
 
 <head>
-    <!-- Start cookieyes banner --><script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/656733d89b24b27671b83202/script.js"></script><!-- End cookieyes banner -->
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
